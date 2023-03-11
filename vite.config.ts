7e667e9a113d2d6dce83b817f9f0e0949225e1fa
Chia Kang Jin <xiankai@gmail.com>
2023-03-11 18:00:38 +0900

    Add nice support for markdown from a vite plugin (text not proofread!!!)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import mdPlugin, { Mode } from "vite-plugin-markdown";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [mdPlugin({ mode: [Mode.HTML] }), tsconfigPaths(), react()],
   server: {
     port: 3000,
   },
