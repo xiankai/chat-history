52243372e859f48954407e4777707e3680bee3cd
Chia Kang Jin <xiankai@gmail.com>
2023-06-18 18:35:26 +0900

    also fix for text alignment

--- src/components/viewer/Messenger/TextMessage.tsx
+++ src/components/viewer/Messenger/TextMessage.tsx
@@ -4,13 +4,21 @@ export const TextMessage = ({
   isRecipient,
   message,
 }: {
-  isRecipient: boolean,
-  message: string,
+  isRecipient: boolean;
+  message: string;
 }) => {
-  return <div className={`
-    ${isRecipient ? "bg-gray-300 text-black" : "bg-blue-500 text-white"}
+  return (
+    <div
+      className={`
+    ${
+      isRecipient
+        ? "bg-gray-300 text-black text-left"
+        : "bg-blue-500 text-white text-right"
+    }
     rounded-3xl pt-2 pb-2 pl-3 pr-3
-  `}>
-    {fixMessengerExport(message)}
-  </div>
-}
+  `}
+    >
+      {fixMessengerExport(message)}
+    </div>
+  );
+};
