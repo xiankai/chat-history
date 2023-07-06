5862c4923e3f5b54d4e75351924a84f9f370de3b
Chia Kang Jin <xiankai@gmail.com>
2023-07-06 21:10:03 +0900

    project url for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/chat-history/",
   plugins: [mdPlugin({ mode: [Mode.HTML] }), tsconfigPaths(), react()],
   server: {
     port: 3000,
